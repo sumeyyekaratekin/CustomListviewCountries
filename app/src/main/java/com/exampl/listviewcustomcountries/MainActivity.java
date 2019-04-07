2148d5f25112492ac0470a19889d7e84e07286bc
Sümeyye Karatekin <nsmyyu@gmail.com>
2019-04-07 16:14:02 +0300

    ilk commit

---
+++ app/src/main/java/com/exampl/listviewcustomcountries/MainActivity.java
@@ -0,0 +1,32 @@
+package com.exampl.listviewcustomcountries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] ulkeler =
+            {"Türkiye", "Almanya", "Avusturya", "Amerika","İngiltere",
+                    "Macaristan", "Yunanistan", "Rusya", "Suriye", "İran", "Irak",
+                    "Şili", "Brezilya", "Japonya", "Portekiz", "İspanya",
+                    "Makedonya", "Ukrayna", "İsviçre"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //(A) adımı
+        ListView listemiz=(ListView) findViewById(R.id.listView1);
+
+        //(B) adımı
+        ArrayAdapter<String> veriAdaptoru=new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, android.R.id.text1, ulkeler);
+
+        //(C) adımı
+        listemiz.setAdapter(veriAdaptoru);
+
+    }
+}
